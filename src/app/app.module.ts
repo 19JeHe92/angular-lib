b8261dc0762a637693a8539b403d57c1b904320f
Jessica Hempel <jessica.hempel@avency.de>
2020-06-30 20:09:27 +0200

    feat: configure app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TodoModule } from '../../lib/src/todo.module';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    TodoModule.forRoot({brand: 'Brand Name'})
   ],
   providers: [],
   bootstrap: [AppComponent]
